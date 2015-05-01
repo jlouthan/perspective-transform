c64f0eb2684451ce4b71f99864a8a9e06cb3d4e8
Jennifer Louthan <jenny@uncorkedstudios.com>
2015-04-30 17:44:41 -0700

    mat and inverse mat calculation uses same function

--- dist/perspective-transform.js
+++ dist/perspective-transform.js
@@ -20,7 +20,12 @@
 		return array;
 	}
 
-	function getNormalizationCoefficients(srcPts, dstPts){
+	function getNormalizationCoefficients(srcPts, dstPts, isInverse){
+		if(isInverse){
+			var tmp = dstPts;
+			dstPts = srcPts;
+			srcPts = tmp;
+		}
 		var r1 = [srcPts[0], srcPts[1], 1, 0, 0, 0, -1*dstPts[0]*srcPts[0], -1*dstPts[0]*srcPts[1]];
 		var r2 = [0, 0, 0, srcPts[0], srcPts[1], 1, -1*dstPts[1]*srcPts[0], -1*dstPts[1]*srcPts[1]];
 		var r3 = [srcPts[2], srcPts[3], 1, 0, 0, 0, -1*dstPts[2]*srcPts[2], -1*dstPts[2]*srcPts[3]];
@@ -45,32 +50,6 @@
 		return getArrayFromMatSize8(matX);
 	}
 
-	function getNormalizationInverseCoefficients(srcPts, dstPts){
-
-		var r1 = [dstPts[0], dstPts[1], 1, 0, 0, 0, -1*srcPts[0]*dstPts[0], -1*srcPts[0]*dstPts[1]];
-		var r2 = [0, 0, 0, dstPts[0], dstPts[1], 1, -1*srcPts[1]*dstPts[0], -1*srcPts[1]*dstPts[1]];
-		var r3 = [dstPts[2], dstPts[3], 1, 0, 0, 0, -1*srcPts[2]*dstPts[2], -1*srcPts[2]*dstPts[3]];
-		var r4 = [0, 0, 0, dstPts[2], dstPts[3], 1, -1*srcPts[3]*dstPts[2], -1*srcPts[3]*dstPts[3]];
-		var r5 = [dstPts[4], dstPts[5], 1, 0, 0, 0, -1*srcPts[4]*dstPts[4], -1*srcPts[4]*dstPts[5]];
-		var r6 = [0, 0, 0, dstPts[4], dstPts[5], 1, -1*srcPts[5]*dstPts[4], -1*srcPts[5]*dstPts[5]];
-		var r7 = [dstPts[6], dstPts[7], 1, 0, 0, 0, -1*srcPts[6]*dstPts[6], -1*srcPts[6]*dstPts[7]];
-		var r8 = [0, 0, 0, dstPts[6], dstPts[7], 1, -1*srcPts[7]*dstPts[6], -1*srcPts[7]*dstPts[7]];
-
-		var matA = math.matrix([r1, r2, r3, r4, r5, r6, r7, r8]);
-		var matB = math.matrix(srcPts);
-	
-		try{
-	    	var matC = math.inv(math.multiply(math.transpose(matA), matA));
-		}catch(e){
-	    	console.log(e);
-	    	return [1,0,0,0,1,0,0,0];
-		}
-		var matD = math.multiply(matC, math.transpose(matA));
-		var matX = math.multiply(matD, matB);
-
-		return getArrayFromMatSize8(matX);
-	}
-
 	function PerspT(srcPts, dstPts){
 		if(window === this || this === undefined) {
 			return new PerspT(srcPts, dstPts);
@@ -78,8 +57,8 @@
 
 		this.srcPts = srcPts;
 		this.dstPts = dstPts;
-		this.coeffs = getNormalizationCoefficients(this.srcPts, this.dstPts);
-		this.coeffsInv = getNormalizationInverseCoefficients(this.srcPts, this.dstPts);
+		this.coeffs = getNormalizationCoefficients(this.srcPts, this.dstPts, false);
+		this.coeffsInv = getNormalizationCoefficients(this.srcPts, this.dstPts, true);
 
 		return this;
 	}
