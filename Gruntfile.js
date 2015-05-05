52961ea8614fc8aa43a268596224012b8f628581
Jennifer Louthan <jenny@uncorkedstudios.com>
2015-05-05 11:40:59 -0700

    adding Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		jshint: {
+			all: ['Gruntfile.js', 'src/**/*.js'],
+			options: {
+				jshintrc: true
+			}
+		},
+
+		uglify: {
+			dist: {
+				files: {
+					'dist/perspective-transform.min.js': 'src/perspective-transform.js'
+				}
+			},
+			options: {
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+				mangle: true,
+				preserveComments: 'some'
+			}
+		}
+
+	});
+
+	require('load-grunt-tasks')(grunt);
+	grunt.registerTask('default', ['jshint', 'uglify']);
+}
